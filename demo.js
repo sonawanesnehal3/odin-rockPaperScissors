db1aa13046b2ea0c221b4937c40a1f801828200a
Snehal Sonawane <sonawane@Snehals-MacBook-Pro.local>
2024-02-13 10:59:19 +0530

    UI changes for the game

--- demo.js
+++ demo.js
@@ -1,5 +1,22 @@
 
 let choices = ['rock', 'paper', 'scissor'];
+let playerScore = 0;
+let computerScore = 0;
+let computerChoice;
+
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorButton = document.querySelector('#scissor');
+const playerDecision = document.querySelector('.player-decision');
+const computerDecision = document.querySelector('.computer-decision');
+const result = document.querySelector('.winner');
+const playerScoreElement = document.querySelector('.player-score');
+const computerScoreElement = document.querySelector('.computer-score');
+const restartButton = document.querySelector('#restart');
+
+playerScoreElement.textContent = playerScore;
+computerScoreElement.textContent = computerScore;
+
 
 function getComputerChoice(){
     //console.log("Hello World!!");
@@ -7,45 +24,67 @@ function getComputerChoice(){
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-//const playerSelection = "rock";
-//console.log("Player choice is : " , playerSelection);
-//let computerChoice = getComputerChoice();
-let playerscore = 0;
+rockButton.addEventListener('click', function player(){
+    if (playerScoreElement.textContent < 5 && computerScoreElement.textContent < 5){
+        playerSelection = rockButton.value;
+        playRound(playerSelection); 
+    }
+});
+
+paperButton.addEventListener('click', function player(){
+    if (playerScoreElement.textContent < 5 && computerScoreElement.textContent < 5){
+        playerSelection = paperButton.value;
+        playRound(playerSelection); 
+    }
+});
+
+scissorButton.addEventListener('click', function player(){
+    if (playerScoreElement.textContent < 5 && computerScoreElement.textContent < 5){
+        playerSelection = scissorButton.value;
+        playRound(playerSelection); 
+    }
+});
+
+restartButton.addEventListener('click', function restart(){
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreElement.textContent = playerScore;
+    computerScoreElement.textContent = computerScore;
+    result.textContent = "";
+    result.setAttribute('style','color: black')
+    playerDecision.textContent = "";
+    computerDecision.textContent = "";
+})
 
 function playRound(playerSelection){
-    let computerChoice = getComputerChoice();
-    console.log("Computer choice is : " , computerChoice);
+    computerChoice = getComputerChoice();
+    playerDecision.textContent = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    computerDecision.textContent = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+
     if(computerChoice == playerSelection){
-        console.log("It's a tie!");
-        return playerscore;
+        result.textContent = "It's a tie!";
     }
     else if((computerChoice === 'paper' && playerSelection === 'rock') || (computerChoice === 'rock', playerSelection === 'scissor') || (computerChoice === 'scissor' && playerSelection === 'paper')){
-        console.log("Computer won the round!");
-        return playerscore;
+        result.textContent = "Computer won the round!";
+        computerScore++;
+        computerScoreElement.textContent = computerScore;
     }
-
     else if((computerChoice === 'rock' && playerSelection === 'paper') || (computerChoice === 'scissor' && playerSelection === 'rock') || (computerChoice === 'paper' && playerSelection === 'scissor')){
-        console.log("You won the round!");
-        return playerscore++;
+        result.textContent = "You won the round!";
+        playerScore++;
+        playerScoreElement.textContent = playerScore;
     }
+
+    checkWinner();
 }
 
-//console.log(playRound(playerSelection,computerChoice));
-function playGame(){
-    
-    for (let i = 0; i < 5; i++) {
-        console.log("Playcount : " , i+1);
-        let playerSelection = prompt("What's your choice? ");
-        console.log("Player choice is : " , playerSelection);
-        playRound(playerSelection);
+function checkWinner(){
+    if(playerScore == 5){
+        result.textContent = "YOU WON THE GAME!!";
+        result.setAttribute("style", "color: green");
     }
-    console.log("Player score = " , playerscore);
-    if(playerscore > 2){
-            console.log("You won the Game!");
+    else if(computerScore == 5){
+        result.textContent = "COMPUTER WON THE GAME!!"
+        result.setAttribute("style", "color: red");
     }
-    else{
-            console.log("Sorry! You could not win!");
-    }  
 }
-
-playGame();
